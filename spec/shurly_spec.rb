73f1632a952a0e741d1a6ad34bbc1a3ecfb08280
Tim Preston <tprest@gmail.com>
2011-04-07 21:53:08 +1000

    Spec'ing out the admin routes

--- spec/shurly_spec.rb
+++ spec/shurly_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
-describe 'Shurly' do
+describe Sinatra::Application do
   describe 'GET /' do
     it 'redirects to home' do
       get '/'
       follow_redirect!
       last_request.url.should eql 'http://www.amc.org.au/'
     end
+  end
+  
+  describe 'GET /admin' do
+    it 'renders ok' do
+      get '/admin'
+      last_response.should be_ok
+    end
+    
+    it 'displays a list of long and short urls'
+    # maybe split this out into an integration test
   end
 end
