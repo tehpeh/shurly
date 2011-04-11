152ecd56bba9f8ba460f49ff510fce8d2cbf65ac
Tim Preston <tim.preston@amc.org.au>
2011-04-11 14:16:25 +1000

    Added logging to log/ directory.

--- lib/security.rb
+++ lib/security.rb
@@ -5,10 +5,15 @@ module Shurly
   protected
 
     # Set enviroment variable WHITELIST_IP as comma delimited list of IP addresses
-    def protected_by_ip
+    # Pass your logger to have a message logged
+    def protected_by_ip(logger = nil)
       unless ENV['WHITELIST_IP'].nil?
-        redirect '/' unless ENV['WHITELIST_IP'].split(',').include?(request.env['HTTP_X_REAL_IP'])
+        unless ENV['WHITELIST_IP'].split(',').include?(request.env['HTTP_X_REAL_IP'])
+          logger.warn "#{request.env['HTTP_X_REAL_IP']} attempted to access a secure area" unless logger.nil?
+          redirect '/'
+        end
       end
     end
+
   end
 end
--- shurly.rb
+++ shurly.rb
@@ -1,12 +1,24 @@
 require 'sinatra'
 require 'haml'
 require 'active_record'
+require 'logger'
 require File.expand_path(File.join(File.dirname(__FILE__), 'lib/security'))
 include Shurly::Security
+
+configure do
+  unless defined?(LOGGER)
+    set :logging, false
+    Dir.mkdir('log') unless File.exists?('log')
+    class ::Logger; alias_method :write, :<<; end
+    LOGGER = ::Logger.new("log/#{ENV['RACK_ENV']}.log",'weekly')
+    use Rack::CommonLogger, LOGGER
+  end
+end
 
 configure :development do |c|
   require "sinatra/reloader"
   #c.dont_reload "log/*"
+  c.also_reload "lib/*.rb"
   #c.also_reload "*.haml"
 end
 
@@ -17,6 +29,6 @@ get '/' do
 end
 
 get '/admin' do
-  protected_by_ip
+  protected_by_ip(LOGGER)
   haml :'admin/index'
 end
